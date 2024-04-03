4f992ca7759ff1fa4c5364d645d09173e882bfde
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-04-03 23:43:13 +0300

    ДЗ. Модуль№4

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,64 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/SearchBox';
-import { nanoid } from 'nanoid';
-
+import SearchBar from './components/SearchBar/SearchBar'
+import LoadMoreButton from './components/LoadMoreButton/LoadMoreButton';
+import { requestImageGallery } from './services/HTTPRequest/HTTPRequest';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import toast, { Toaster } from 'react-hot-toast';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import ImageModal from './components/ImageModal/ImageModal';
+import ReactModal from 'react-modal';
 function App() {
-  const STORAGE_KEY = 'listOfContact';
-  const initialContactList=JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? [
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ];
-  const [contactsList, setContactsList] = useState(initialContactList);
-  const [filter, setFilter] = useState('');
-  const onChangefilter = (event) => { setFilter(event.target.value) };
-  const filteredContactList = contactsList.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
-  const onAddContact = (formData) => {
-    const contactToAdd = { ...formData, id: nanoid() };
-    setContactsList((prevState) => [...prevState, contactToAdd]);
+  const [query, setQuery] = useState('');
+  const [photos, setPhotos] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [totalPageNumber, setTotalPageNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+ReactModal.setAppElement('#root');
+  const onSetQuery = (queryWord) => {
+    setQuery(queryWord); setPageNumber(1); setTotalPageNumber(0); setPhotos(null); 
   };
-  const onDeleteContact = (contactId) => {
-    setContactsList((prevState) => prevState.filter(contact => contact.id !== contactId));
-  };
-  useEffect(() => { localStorage.setItem(STORAGE_KEY, JSON.stringify(contactsList)) });
+  const onSetPageNumber = () => { setPageNumber(pageNumber + 1);  }
+  useEffect(() => {
+    if (query.length === 0) return;
+    async function fetchPhotoGallery() {
+      try {
+        setIsLoading(true);
+        const data = await requestImageGallery(query, pageNumber);
+
+        setTotalPageNumber(data.total_pages);
+        if (data.results && data.results.length > 0) {
+          setPhotos(prevPhotos => {
+            if (!Array.isArray(prevPhotos)) {
+              return data.results;
+            }
+            return [...prevPhotos, ...data.results];
+          
+          })
+        } else { toast('Sorry, there are no images matching your search query. Please try again with anover search term!'); }
+              }
+      catch (error) { setIsError(true); }
+      finally { setIsLoading(false); }
+    }
+    fetchPhotoGallery();
+  }, [query, pageNumber]);
+  const handleCloseModal = () => { setModalIsOpen(false); }
+  const handleImageClick = (photo) => { setSelectedPhoto(photo); setModalIsOpen(true); }
   return (
     <>
       <div className='container'>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={onAddContact}/>
-      <SearchBox inputValue={filter} handleChange={onChangefilter} />
-      <ContactList listOfcontacts={filteredContactList} onDeleteContact={onDeleteContact}  />
+      <h1>World of Photo</h1>
+        <Toaster  position="top-center" reverseOrder={true}/>
+        <SearchBar onSetQuery={onSetQuery} />
+        {isError && <ErrorMessage />}
+        {Array.isArray(photos) && <ImageGallery photos={photos} handleImageClick={handleImageClick} />}
+        {selectedPhoto && <ImageModal isOpen={modalIsOpen} handleCloseModal={handleCloseModal} selectedPhoto={selectedPhoto} />}
+        {isLoading && <Loader/>}
+        {totalPageNumber -pageNumber >= 1 && <LoadMoreButton handleClick={onSetPageNumber} />};
     </div>
       
     </>
